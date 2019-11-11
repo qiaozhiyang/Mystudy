f264adbf32f4ed1435e671c1463d84958d7df2f3
joe <15515790378@163.com>
2019-11-11 17:58:41 +0800

    测试类

---
+++ Day01/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author qiaozy@iri.cn
+ * @date 2019-11-11 下午 17:48
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
